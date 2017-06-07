394cbba5bcfa00be82585c8b17ed910dcda6eca8
crabcrabcam <cam12win@gmail.com>
2017-06-07 09:22:16 +0100

    Fixed QIcon for lesson 2
    Needed to import it from the QtGui

--- pyqt5_lesson_02.py
+++ pyqt5_lesson_02.py
@@ -13,16 +13,17 @@
 import sys
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
+from PyQt5.QtGui import QIcon
 
-class window(QMainWindow):
+class Window(QMainWindow):
 
     def __init__(self):
-        super(window, self).__init__()
+        super(Window, self).__init__()
         self.setGeometry(50, 50, 500, 300)
         self.setWindowTitle('pyqt5 Tut')
-        # self.setWindowIcon(QIcon('pic.png'))
+        self.setWindowIcon(QIcon('pic.png'))
         self.show()
 
 app = QApplication(sys.argv)
-Gui = window()
+Gui = Window()
 sys.exit(app.exec_())
